627b25caa87777980603f182c6c762df51260508
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-12-01 15:37:24 +0100

    Add .NET Aspire

--- src/LoggingAndMonitoringWithDotNet/Program.cs
+++ src/LoggingAndMonitoringWithDotNet/Program.cs
@@ -1,11 +1,25 @@
 using LoggingAndMonitoringWithDotNet.Modules;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Register Modules
 builder.RegisterModules();
+builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
+{
+config.SetAzureTokenCredential(new DefaultAzureCredential());
+});
+
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+  ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 app.ConfigureModules();
 
