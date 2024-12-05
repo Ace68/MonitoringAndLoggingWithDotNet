0e0c04826fe894fc3b0652ba9648bf059c326bb8
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-12-05 16:15:02 +0100

    Add Polly with Retry, TimeOut, CircuitBreaker

---
+++ src/BrewUp.CircuitBreaker/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+using Polly.Timeout;
+
+var services = new ServiceCollection();
+
+const string key = "Retry-Timeout-CircuitBreaker";
+const string httpClientName = "BrewUpClient";
+
+// Polly documentation
+// Retry: https://www.pollydocs.org/strategies/retry.html
+// Timeout: https://www.pollydocs.org/strategies/timeout.html
+// CircuitBreaker: https://www.pollydocs.org/strategies/circuit-breaker.html
+services.AddResiliencePipeline(key, static builder =>
+    {
+        builder.AddRetry(new RetryStrategyOptions
+        {
+            ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>()
+        });
+
+        builder.AddTimeout(TimeSpan.FromSeconds(1.5));
+
+        builder.AddCircuitBreaker(new CircuitBreakerStrategyOptions
+        {
+            FailureRatio = 0.5,
+            SamplingDuration = TimeSpan.FromSeconds(10),
+            MinimumThroughput = 8,
+            BreakDuration = TimeSpan.FromSeconds(30)
+        });
+    })
+    .AddHttpClient();
+
+var serviceProvider = services.BuildServiceProvider();
+var clientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+var httpClient = clientFactory.CreateClient(httpClientName);
+
+for (var i = 0; i < 30; i++)
+{
+    Console.WriteLine($"Attempt # {i + 1}");
+    var response = await httpClient.GetAsync("https://brewupapi.ambitiousocean-9c685401.italynorth.azurecontainerapps.io/v1/brewup");
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+    
+    Thread.Sleep(500);
+    
+    if (i < 29)
+        Console.Clear();
+}
+
+
