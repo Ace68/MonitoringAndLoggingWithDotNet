c5087aa7bc09ad6375ef1b25f66f6fc47c9657f5
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-12-08 10:10:19 +0100

    Set ApplicationInsights connection

--- src/LoggingAndMonitoringWithDotNet/Endpoints/BrewUpEndpoints.cs
+++ src/LoggingAndMonitoringWithDotNet/Endpoints/BrewUpEndpoints.cs
@@ -4,25 +4,30 @@ namespace LoggingAndMonitoringWithDotNet.Endpoints;
 
 public static class BrewUpEndpoints
 {
-    public static IEndpointRouteBuilder MapBrewUpEndpoints(this IEndpointRouteBuilder endpoints)
-    {
-        var group = endpoints.MapGroup("/v1/brewup/")
-            .WithTags("Sales");
-        
-        group.MapGet("/", HandleGetOrders)
-            .Produces(StatusCodes.Status400BadRequest)
-            .Produces(StatusCodes.Status200OK)
-            .WithName("GetSalesOrders");
+  public static IEndpointRouteBuilder MapBrewUpEndpoints(this IEndpointRouteBuilder endpoints)
+  {
+    var group = endpoints.MapGroup("/v1/brewup/")
+        .WithTags("Sales");
 
-        return endpoints;
-    }
-    
-    private static Task<IResult> HandleGetOrders(
-        CancellationToken cancellationToken)
-    {
-        cancellationToken.ThrowIfCancellationRequested();
-        
-        var orders = new List<SalesOrder>
+    group.MapGet("/orders", HandleGetOrders)
+        .Produces(StatusCodes.Status400BadRequest)
+        .Produces(StatusCodes.Status200OK)
+        .WithName("GetOrders");
+
+    group.MapGet("/beers", HandleGetBeers)
+      .Produces(StatusCodes.Status400BadRequest)
+      .Produces(StatusCodes.Status200OK)
+      .WithName("GetBeers");
+
+    return endpoints;
+  }
+
+  private static Task<IResult> HandleGetOrders(
+      CancellationToken cancellationToken)
+  {
+    cancellationToken.ThrowIfCancellationRequested();
+
+    var orders = new List<SalesOrder>
         {
             new()
             {
@@ -74,6 +79,32 @@ public static class BrewUpEndpoints
             }
         };
 
-        return Task.FromResult(Results.Ok(orders));
-    }
+    return Task.FromResult(Results.Ok(orders));
+  }
+
+  private static Task<IResult> HandleGetBeers(
+    CancellationToken cancellationToken)
+  {
+    cancellationToken.ThrowIfCancellationRequested();
+
+    var beers = new List<Beer>
+      {
+        new()
+        {
+          BeerId = Guid.NewGuid().ToString(),
+          Name = "Muflone IPA",
+          Quantity = new Quantity(20, "Lt"),
+          Price = new Price(5, "EUR")
+        },
+        new()
+        {
+          BeerId = Guid.NewGuid().ToString(),
+          Name = "Muflone Weiss",
+          Quantity = new Quantity(10, "Lt"),
+          Price = new Price(5, "EUR")
+        }
+      };
+
+    return Task.FromResult(Results.Ok(beers));
+  }
 }
--- src/LoggingAndMonitoringWithDotNet/Modules/MetricsModule.cs
+++ src/LoggingAndMonitoringWithDotNet/Modules/MetricsModule.cs
@@ -1,52 +1,30 @@
 ﻿using Azure.Identity;
-using Azure.Monitor.OpenTelemetry.AspNetCore;
-using OpenTelemetry.Instrumentation.Runtime;
-using OpenTelemetry.Metrics;
 
 namespace LoggingAndMonitoringWithDotNet.Modules;
 
 public class MetricsModule : IModule
 {
-    public bool IsEnabled => true;
-    public int Order => 0;
-    
-    public IServiceCollection Register(WebApplicationBuilder builder)
+  public bool IsEnabled => true;
+  public int Order => 0;
+
+  public IServiceCollection Register(WebApplicationBuilder builder)
+  {
+    builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
     {
-        builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
-        {
-            config.SetAzureTokenCredential(new DefaultAzureCredential());
-        });
+      config.SetAzureTokenCredential(new DefaultAzureCredential());
+    });
 
-        builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
-        {
-            ConnectionString = builder.Configuration["AzureMonitor:ConnectionString"]
-        });
-        
-        // Configure the OpenTelemetry meter provider to add runtime instrumentation.
-        builder.Services.ConfigureOpenTelemetryMeterProvider((_, provider) =>  provider.AddRuntimeInstrumentation()); 
-
-        builder.Services.AddOpenTelemetry()
-            // Configure OpenTelemetry to use Azure Monitor.
-            .UseAzureMonitor()
-            // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
-            .WithMetrics(meterProviderBuilder =>
-            {
-                // Metrics provider from OpenTelemetry
-                meterProviderBuilder.AddAspNetCoreInstrumentation();
-                meterProviderBuilder.AddMeter();
-                // Metrics provides by ASP.NET Core in .NET 8
-                meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting");
-                meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
-                meterProviderBuilder.AddPrometheusExporter();
-            });
-		
-        return builder.Services;
-    }
-
-    public WebApplication Configure(WebApplication app)
+    builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
     {
-        app.MapPrometheusScrapingEndpoint();
-        
-        return app;
-    }
+      ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+    });
+
+    return builder.Services;
+  }
+
+  public WebApplication Configure(WebApplication app)
+  {
+
+    return app;
+  }
 }
