f9a8cc3bed6fd4ebfeafb9224a3d77e5af2f2b18
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-12-09 17:01:46 +0100

    Move .NET Aspire regstration into AspireModule

---
+++ src/LoggingAndMonitoringWithDotNet/Modules/AspireModule.cs
@@ -0,0 +1,16 @@
+﻿namespace LoggingAndMonitoringWithDotNet.Modules;
+
+public class AspireModule : IModule
+{
+  public bool IsEnabled => true;
+  public int Order => 0;
+
+  public IServiceCollection Register(WebApplicationBuilder builder)
+  {
+    builder.AddServiceDefaults();
+
+    return builder.Services;
+  }
+
+  public WebApplication Configure(WebApplication app) => app;
+}
--- src/LoggingAndMonitoringWithDotNet/Modules/MetricsModule.cs
+++ src/LoggingAndMonitoringWithDotNet/Modules/MetricsModule.cs
@@ -1,41 +1,51 @@
-﻿using Azure.Monitor.OpenTelemetry.AspNetCore;
-using OpenTelemetry.Instrumentation.Runtime;
+﻿using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 using OpenTelemetry.Metrics;
 
 namespace LoggingAndMonitoringWithDotNet.Modules;
 
 public class MetricsModule : IModule
 {
-    public bool IsEnabled => true;
-    public int Order => 0;
-    
-    public IServiceCollection Register(WebApplicationBuilder builder)
-    {
-        // Configure the OpenTelemetry meter provider to add runtime instrumentation.
-        builder.Services.ConfigureOpenTelemetryMeterProvider((_, provider) =>  provider.AddRuntimeInstrumentation()); 
+  public bool IsEnabled => true;
+  public int Order => 0;
 
-        builder.Services.AddOpenTelemetry()
-            // Configure OpenTelemetry to use Azure Monitor.
-            .UseAzureMonitor()
-            // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
-            .WithMetrics(meterProviderBuilder =>
-            {
-                // Metrics provider from OpenTelemetry
-                meterProviderBuilder.AddAspNetCoreInstrumentation();
-                meterProviderBuilder.AddMeter();
-                // Metrics provides by ASP.NET Core in .NET 8
-                meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting");
-                meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
-                meterProviderBuilder.AddPrometheusExporter();
-            });
-		
-        return builder.Services;
-    }
-
-    public WebApplication Configure(WebApplication app)
+  public IServiceCollection Register(WebApplicationBuilder builder)
+  {
+    builder.Services.Configure<Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration>(config =>
     {
-        app.MapPrometheusScrapingEndpoint();
-        
-        return app;
-    }
+      config.SetAzureTokenCredential(new DefaultAzureCredential());
+    });
+
+    builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+    {
+      ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+    });
+
+    // Configure the OpenTelemetry meter provider to add runtime instrumentation.
+    builder.Services.ConfigureOpenTelemetryMeterProvider((_, provider) => provider.AddRuntimeInstrumentation());
+
+    builder.Services.AddOpenTelemetry()
+        // Configure OpenTelemetry to use Azure Monitor.
+        .UseAzureMonitor()
+        // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+        .WithMetrics(meterProviderBuilder =>
+        {
+          // Metrics provider from OpenTelemetry
+          meterProviderBuilder.AddAspNetCoreInstrumentation();
+          meterProviderBuilder.AddMeter();
+          // Metrics provides by ASP.NET Core in .NET 8
+          meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting");
+          meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Server.Kestrel");
+          meterProviderBuilder.AddPrometheusExporter();
+        });
+
+    return builder.Services;
+  }
+
+  public WebApplication Configure(WebApplication app)
+  {
+    app.MapPrometheusScrapingEndpoint();
+
+    return app;
+  }
 }
